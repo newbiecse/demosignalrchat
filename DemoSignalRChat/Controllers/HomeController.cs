0db8fa65c48e44d9e8604599cd685b1b5084ce20
newbiecse <NewbieCSE@gmail.com>
2014-02-20 16:28:13 +0000

    post status ajax

--- DemoSignalRChat/Controllers/HomeController.cs
+++ DemoSignalRChat/Controllers/HomeController.cs
@@ -4,6 +4,12 @@ using DemoSignalRChat.ViewModels;
 using DemoSignalRChat.DAL;
 using Microsoft.AspNet.Identity;
 using System.Net;
+using NSoup.Nodes;
+using System.Collections.Generic;
+using NSoup.Select;
+using DemoSignalRChat.Preview;
+using System;
+using System.Linq;
 
 namespace DemoSignalRChat.Controllers
 {
@@ -25,18 +31,73 @@ namespace DemoSignalRChat.Controllers
             var friendList = new FriendListViewModel(user.UserId);
             ViewBag.FriendList = friendList.FriendList;
 
-            string responseString;
-            using (var client = new WebClient())
+            var client = new WebClient();
+            string html = client.DownloadString("http://www.aao.hcmut.edu.vn/");
+            Document doc = NSoup.NSoupClient.Parse(html);
+
+            Elements imgs = doc.Select("img");
+
+            List<Img> images = new List<Img>();
+            foreach (var i in imgs)
             {
-                responseString = client.DownloadString("http://vnexpress.net/");
+                string imgWidth = i.Attr("Width");
+                int width = 0;
+                Int32.TryParse(imgWidth, out width);
+
+                string imgHeight = i.Attr("Height");
+                int height = 0;
+                Int32.TryParse(imgHeight, out height);
+
+                images.Add(new Img { Src = i.Attr("src"), Width = width, Height = height });
             }
+
+            List<Img> imagesSorted = (from i in images
+                                      select i).OrderByDescending(i => i.Height * i.Width).ToList();
+
+            ViewBag.imgSrcs = imagesSorted;
 
             //Document doc = NSoup.NSoupClient.Parse(responseString);
             //string imgSrc = doc.Select("");
 
             //int x = 9;
 
+
             return View(user);
+        }
+
+
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public JsonResult PostLink(string Content)
+        {
+            var client = new WebClient();
+            //client.DownloadData
+            string html = client.DownloadString(Content);
+            Document doc = NSoup.NSoupClient.Parse(html);
+
+            Elements imgs = doc.Select("img");
+
+            List<Img> images = new List<Img>();
+            foreach (var i in imgs)
+            {
+                string imgWidth = i.Attr("Width");
+                int width = 0;
+                Int32.TryParse(imgWidth, out width);
+
+                string imgHeight = i.Attr("Height");
+                int height = 0;
+                Int32.TryParse(imgHeight, out height);
+
+                images.Add(new Img { Src = i.Attr("src"), Width = width, Height = height });
+            }
+
+            List<Img> imagesSorted = (from i in images
+                                      select i).OrderByDescending(i => i.Height * i.Width).ToList();
+
+            var src = imagesSorted.First().Src;
+
+            //return Json(src, JsonRequestBehavior.AllowGet);
+            return Json(src);
         }
 
         public ActionResult About()
