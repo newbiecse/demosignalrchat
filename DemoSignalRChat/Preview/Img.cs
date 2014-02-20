59fdd8ff866ca35a43874f1ae1ff23c9021acd8d
newbiecse <NewbieCSE@gmail.com>
2014-02-20 17:00:34 +0000

    complete ajax post status but useless :)

---
+++ DemoSignalRChat/Preview/Img.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoSignalRChat.Preview
+{
+    public class Img
+    {
+        public string Src { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
