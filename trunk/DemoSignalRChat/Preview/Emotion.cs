d295910e52e5d4298269249c9fee33dd1495a9ac
newbiecse@gmail.com <newbiecse@gmail.com@8564aa9e-a5ab-ab6e-12a6-53695b5d8904>
2014-03-11 13:29:02 +0000

    emotions

---
+++ trunk/DemoSignalRChat/Preview/Emotion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoSignalRChat.Preview
+{
+    public static class Emotions
+    {
+        private static string InsertImage(string emotionName)
+        {
+            var url = "/images/Emotions/";
+            return "<img src='" + url + emotionName + "' />";
+        }
+
+        //public static string ReplaceEmotion(string specialKey, string image)
+        //{
+        //    return 
+        //}
+
+        public static string ReplaceEmotions(string text)
+        {
+
+            return text.Replace(":)", Emotions.InsertImage("happy.gif"))
+                        .Replace(":(", Emotions.InsertImage("sad.gif"))
+                        .Replace(";)", Emotions.InsertImage("winking.gif"))
+                        .Replace(";D", Emotions.InsertImage("big_grin.gif"))
+
+                        .Replace(":-c", Emotions.InsertImage("call_me.gif"))
+                        .Replace(":-*", Emotions.InsertImage("kiss.gif"))
+                        .Replace(":-SS", Emotions.InsertImage("applause.gif"))
+                        .Replace(":v", Emotions.InsertImage("pacman.png"))
+                        .Replace("^_^", Emotions.InsertImage("kiki.png"))
+                        .Replace("B|", Emotions.InsertImage("sunglasses.png"));                      
+        }
+    }
+}
