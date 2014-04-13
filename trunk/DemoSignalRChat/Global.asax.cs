530041afdcbb71bc2268231965a752018caa40d1
newbiecse@gmail.com <newbiecse@gmail.com@8564aa9e-a5ab-ab6e-12a6-53695b5d8904>
2014-04-13 07:28:55 +0000

    fix layout + add jquery mobile

--- trunk/DemoSignalRChat/Global.asax.cs
+++ trunk/DemoSignalRChat/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.WebPages;
 
 namespace DemoSignalRChat
 {
@@ -16,6 +17,26 @@ namespace DemoSignalRChat
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            BundleMobileConfig.RegisterBundles(BundleTable.Bundles);
+
+            DisplayModeProvider.Instance.Modes.Insert(0, new DefaultDisplayMode("iphone")
+            {
+                ContextCondition = Context =>
+                                Context.Request.Browser["HardwareModel"] == "iPhone"
+            });
+
+            DisplayModeProvider.Instance.Modes.Insert(1, new DefaultDisplayMode("android")
+            {
+                ContextCondition = Context =>
+                                Context.Request.Browser["PlatformName"] == "Android"
+            });
+
+            DisplayModeProvider.Instance.Modes.Insert(2, new DefaultDisplayMode("mobile")
+            {
+                ContextCondition = Context =>
+                                Context.Request.Browser["IsMobile"] == "True"
+            });
+
         }
     }
 }
